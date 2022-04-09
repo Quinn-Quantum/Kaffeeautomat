3f20e79ec1aea027a95cf0146e1265fe284c72ec
Quinn-Quantum <79361383+Quinn-Quantum@users.noreply.github.com>
2022-04-09 12:34:55 +0200

    init Gui

---
+++ Automat/DB_Anbindung.py
@@ -0,0 +1,15 @@
+# Modul für die Kommunikation mit einer MySQL Datenbank
+import mysql.connector
+try:
+  #Aufbau einer Verbindung
+  db = mysql.connector.connect(
+      host="localhost",  # Servername
+      user="root",  # Benutzername
+      password=""  # Passwort
+  )
+  # Ausgabe des Hashwertes des initialisierten Objektes
+  print(db)
+# Wenn ein Fehler vom Typ "mysql.connector.errors.ProgrammingError" aufgetreten ist...
+except mysql.connector.errors.ProgrammingError:
+  # Ausgabe einer Fehlermeldung auf der Konsole
+  print("Fehler beim Aufbau der DB Verbindung aufgetreten!")
