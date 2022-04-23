cb25e8bb559398ec850a16067c427c27ccff3242
Quinn-Quantum <79361383+Quinn-Quantum@users.noreply.github.com>
2022-04-23 18:19:43 +0200

    init class Bestand

--- Automat/Verwaltung.py
+++ Automat/Verwaltung.py
@@ -68,4 +68,33 @@ class Kaffee:
     def setMenge3(self,menge):
         self.__menge3 = menge
 
+class Bestand:
+    __bEspresso = 0
+    __bMilchschaum = 0
+    __bHeißeMilch = 0
 
+    def __init__(self):
+        self.__bEspresso = 0
+        self.__bMilchschaum = 0
+        self.__bHeißeMilch = 0
+
+    # get
+    def getBEspresso(self):
+        return self.__bEspresso
+
+    def getBMilchschaum(self):
+        return self.__bMilchschaum
+
+    def getBHeißeMilch(self):
+        return self.__bHeißeMilch
+
+    # set
+
+    def setBEspresso(self, bEspresso):
+        self.__bEspresso = bEspresso
+
+    def setBMilchschaum(self, bMilchschaum):
+        self.__bMilchschaum = bMilchschaum
+
+    def setBHeißeMilch(self, bHeißeMilch):
+        self.__bHeißeMilch = bHeißeMilch
